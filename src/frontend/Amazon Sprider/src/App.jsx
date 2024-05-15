53378f07c85fee6470638730bc085200f34aa52c
Laraib Riaz Siddiqui <laraibriaz8889@gmail.com>
2024-05-15 12:52:03 +0500

    Add dashboard

--- src/frontend/Amazon Sprider/src/App.jsx
+++ src/frontend/Amazon Sprider/src/App.jsx
@@ -11,19 +11,65 @@ import { Login } from "./pages/auth";
 import { Layout } from "./components";
 import { useSelector } from "react-redux";
 
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import ECommerce from './pages/Dashboard/ECommerce';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+
+
+
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<ProtectedRoute />}>
+      {/* <Routes> */}
+        {/* <Route element={<ProtectedRoute />}>
           <Route index path="/" element={<Home />} />
         </Route>
 
         <Route element={<PublicRoute />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-        {/* <Route path="*" element={<Error404 />} /> */}
+          <Route path="/login" element={<Home />} />
+        </Route> */}
+      {/* </Routes> */}
+
+        <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <ECommerce />
+            </>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <>
+              <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Profile />
+            </>
+          }
+        />
+       
+
+        <Route
+          path="/settings"
+          element={
+            <>
+              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Settings />
+            </>
+          }
+        />
+
+
+  
+        
+        
       </Routes>
+        {/* <Route path="*" element={<Error404 />} /> */}
     </BrowserRouter>
   );
 };
---
+++ src/frontend/Amazon Sprider/src/components/Header/DropdownUser.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import UserOne from '../../images/user/user-01.png';
+
+const DropdownUser = () => {
+
+
+  return (
+    <div className="relative">
+      <div className="flex items-center gap-4">
+        <span className="hidden text-right lg:block">
+          <span className="block text-sm font-medium text-black dark:text-white">
+            Thomas Anree
+          </span>
+          <span className="block text-xs">UX Designer</span>
+        </span>
+
+        <span className="h-12 w-12 rounded-full">
+          <img src={UserOne} alt="User" />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default DropdownUser;
