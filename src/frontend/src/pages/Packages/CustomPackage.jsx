7ecb16fa3cf1764e30e77a653f7612578183d7ce
FarzamN <89484118+FarzamN@users.noreply.github.com>
2024-06-06 12:16:42 +0500

    Update CustomPackage.jsx

--- src/frontend/src/pages/Packages/CustomPackage.jsx
+++ src/frontend/src/pages/Packages/CustomPackage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import DefaultLayout from "@/layout/DefaultLayout";
+import { TableContainer } from "@mui/material";
+import Paper from "@mui/material/Paper";
 
-export default function CustomPackage() {
-  return <div>CustomPackage</div>;
-}
+const CustomPackage = () => {
+  return (
+    <div className="w-full">
+      <DefaultLayout>
+        <TableContainer
+          component={Paper}
+          className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark"
+        ></TableContainer>
+      </DefaultLayout>
+    </div>
+  );
+};
+
+export default CustomPackage;
