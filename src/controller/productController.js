7f496fe80705edfe9da4b1c2a85ba57b24c18aa4
FarzamN <frzamn64ml@gmail.com>
2024-09-27 17:23:35 +0500

    Update productController.js

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,3 +1,6 @@
+import asyncHandler from "express-async-handler";
+import { catchErr } from "../configuration/config.js";
+
 export const getProducts = asyncHandler(async (req, res) => {});
 export const getAllProducts = asyncHandler(async (req, res) => {});
 export const deleteProduct = asyncHandler(async (req, res) => {});
