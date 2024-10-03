c86711859f1252d7e414772fdcb53c458e8ec1e2
FarzamN <frzamn64ml@gmail.com>
2024-10-03 14:46:42 +0500

    Update ViewBlogModal.jsx

--- src/frontend/src/components/Modals/ViewBlogModal.jsx
+++ src/frontend/src/components/Modals/ViewBlogModal.jsx
@@ -11,12 +11,9 @@ const ViewBlogModal = ({ data, open, onClose }) => {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box
-        sx={BoxStyle}
-        className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark"
-      >
+      <Box className="rounded-sm border border-stroke bg-white dark:bg-boxdark shadow-default dark:border-strokedark ">
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 p-4">
-          <div className="bg-white p-8 sm:p-10 rounded-lg shadow-lg max-w-lg w-full h-auto overflow-y-auto relative">
+          <div className="bg-white dark:bg-boxdark p-8 sm:p-10 rounded-lg shadow-lg max-w-lg w-full h-auto overflow-y-auto relative">
             <button
               className="absolute top-4 right-4 px-4 py-2 bg-red-600 text-white rounded-full hover:bg-red-700 transition duration-200"
               onClick={onClose}
@@ -26,14 +23,14 @@ const ViewBlogModal = ({ data, open, onClose }) => {
 
             <>
               <img
-                src={data?.imageUrl}
+                src={data?.blogImage}
                 alt="Blog post"
                 className="w-full h-48 sm:h-64 object-cover mb-4 rounded-lg shadow-sm"
               />
               <h2 className="text-xl sm:text-2xl font-bold mb-4 text-gray-800">
-                {data?.title}
+                {data?.blogTitle}
               </h2>
-              <p className="text-gray-600 mb-4">{data?.content}</p>
+              <p className="text-gray-600 mb-4">{data?.blogDescription}</p>
             </>
           </div>
         </div>
